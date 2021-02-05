edcc3732aa2d1070f3b09e541638f98bafcf9c11
Anastasia <timosha020702@yandex.ru>
2021-02-05 11:09:20 +0300

    first

---
+++ DZ1/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ1
+{
+    public class Class1
+    {
+        private int index;
+        private string n;
+        private string m;
+
+        public int Index { get { return index; } set { index = value; } }
+        public string N { get { return n; } set { n = value; } }
+        public string M { get { return m; } set { m = value; } }
+    }
+}
---
+++ DZ1/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DZ1
+{
+ 
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<Class1> class1s = new List<Class1>();
+            Class1 c1 = new Class1();
+            c1.N = "aa";
+            c1.M = "aaa";
+            c1.Index = 1;
+            class1s.Add(c1);
+            Class1 c2 = new Class1();
+            c2.N = "bb";
+            c2.M = "bbb";
+            c2.Index = 2;
+            class1s.Add(c2);
+            Class1 c3 = new Class1();
+            c3.N = "cc";
+            c3.M = "ccc";
+            c3.Index = 3;
+            class1s.Add(c3);
+
+            lvList.ItemsSource =class1s;
+        }
+      
+    }
+}
